a5a7d1be82bcef2e404dde8ffca1c0f6d1237177
Zhoha Damani <zhohadamani@gmail.com>
2022-08-02 14:41:48 -0400

    added styling buttons for the textUtils

--- basics/src/components/TextForm.js
+++ basics/src/components/TextForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import '../styles/TextForm.css';
+
 
 function TextForm(props) {
-    const [text,setText] = useState('Enter your text here');
+    const [text,setText] = useState('');
+    const [style, setStyle] = useState("normal form-control");
 
     // handle upper case button
     const handleUpperCaseClick = () => {
@@ -11,22 +14,38 @@ function TextForm(props) {
       const handleLowerCaseClick = () => {
         setText(text.toLowerCase());
     }
+    // handle clear text
+    const handleClearText = () => {
+        setText("");
+    }
       // handle on change for text area
       const onChangeHandler = (event) => {
       setText(event.target.value);
+    }
+    const handleBoldClick = () => {
+        setStyle("bolder form-control");
+    }
+    const handleItalicClick = () => {
+        setStyle("italic form-control");
     }
     return (
         <>
         <div className='container'>
             <h1>{props.heading}</h1>
+            <div className='container my-3'>
+            <button className='btn btn-dark mx-1 boldIcon' onClick={handleBoldClick}>B</button>
+            <button className='btn btn-outline-dark mx-1 boldIcon' onClick={handleItalicClick}>I</button>
+            </div>
             <div className="mb-3">
-                <textarea className="form-control" id="myText" rows="8" value={text} onChange={onChangeHandler}></textarea>
+                <textarea className={style}  id="myText" rows="8" value={text} onChange={onChangeHandler}></textarea>
             </div>
             <button className='btn btn-primary mx-1' onClick={handleUpperCaseClick}>Convert to uppercase</button>
             <button className='btn btn-primary mx-1' onClick={handleLowerCaseClick}>Convert to lowercase</button>
+           
+            <button className='btn btn-danger mx-1' onClick={handleClearText}>Clear text</button>
         </div>
         <div className='container'>
-            <h1>Your text summary -</h1>
+            <h2>Your text summary -</h2>
             <p>{text.split(" ").length} word found, and {text.length} characters. </p> 
             <p>{0.08 * text.split(" ").length} mins to read it.</p>
             <h2>Preview -</h2>
